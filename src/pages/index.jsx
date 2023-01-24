9180579dcde3d25c1e7482fad290669a3f45faf0
timothe lim <timothe@lempire.co>
2023-01-24 02:07:06 +0100

    Move all css to Styled

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import * as Styled from "./styled";
 
 export default function Home() {
   return (
@@ -10,17 +10,17 @@ export default function Home() {
         <meta name="description" content="Future of fitness apps" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className={styles.main}>
-          <div className={styles.square}>
-            <Image
-              src="/FITSYNC.svg"
-              alt="app-logo"
-              width={150}
-              height={150}
-              priority
-            />
-          </div>
-       </main>
+      <Styled.main>
+        <Styled.square>
+          <Image
+            src="/FITSYNC.svg"
+            alt="app-logo"
+            width={150}
+            height={150}
+            priority
+          />
+        </Styled.square>
+      </Styled.main>
     </>
-  )
+  );
 }
---
+++ src/pages/styled.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const main = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+export const square = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 200px;
+    padding: 25px 10px;
+    transform: translateZ(0);
+    border-radius: 20px;
+    overflow: hidden;
+    box-shadow: 0px 2px 8px -1px #0000001a;
+
+    ::before,
+    ::after {
+        content: '';
+        position: absolute;
+        z-index: -1;
+    }
+    ::before {
+        animation: 6s rotate linear infinite;
+        width: 200%;
+        height: 200%;
+        background: conic-gradient(
+            #ffffff80,
+            #ffffff40,
+            #ffffff30,
+            #ffffff20,
+            #ffffff10,
+            #ffffff10,
+            #ffffff80
+        );
+    }
+    ::after {
+        inset: 0;
+        padding: 1px;
+        border-radius: 20px;
+        background: linear-gradient(0deg, #1D1D1D 0%, #101010 100%);
+        background-clip: content-box;
+    }
+    @keyframes rotate {
+        from {
+            transform: rotate(360deg);
+        }
+        to {
+            transform: rotate(0deg);
+        }
+        }
+`;
+
