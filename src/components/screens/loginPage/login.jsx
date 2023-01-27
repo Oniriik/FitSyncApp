067abb67e1ae96e6b49c0a3f1ef90e250168e317
timothe lim <timothe@lempire.co>
2023-01-27 23:49:03 +0100

    update

--- src/components/screens/loginPage/login.jsx
+++ src/components/screens/loginPage/login.jsx
@@ -1,9 +1,15 @@
 import * as Styled from "./styled";
 import { signIn } from "next-auth/react";
 import { useFormik } from "formik";
-import { Input } from "@chakra-ui/react";
+import { useState } from "react";
+import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
 import { LogoCard } from "@/components/logoCard";
+import { Icon } from "@chakra-ui/react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 export const LoginPage = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
   const formik = useFormik({
     initialValues: {
       username: "",
@@ -26,21 +32,32 @@ export const LoginPage = () => {
         <Input
           id="username"
           name="username"
+          type="text"
           onChange={formik.handleChange}
           value={formik.values.username}
           placeholder="Username"
           focusBorderColor="#E47D0F"
           color="#DDDDDD"
         />
-        <Input
-          id="password"
-          name="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          placeholder="password"
-          focusBorderColor="#E47D0F"
-          color="#DDDDDD"
-        />
+        <InputGroup size="md">
+          <Input
+            id="password"
+            name="password"
+            type={show ? "text" : "password"}
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            placeholder="password"
+            focusBorderColor="#E47D0F"
+            color="#DDDDDD"
+          />
+          <InputRightElement>
+            <Icon
+              as={show ? AiOutlineEye : AiOutlineEyeInvisible}
+              onClick={handleClick}
+            />
+          </InputRightElement>
+        </InputGroup>
+
         <button type="submit" className="btn-primary">
           Submit
         </button>
